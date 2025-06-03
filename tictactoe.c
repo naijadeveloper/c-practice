d281059013dded08ee9b348f05d5f8d0890bfc0a
Enoch Enujiugha <naijadeveloper@gmail.com>
2025-06-03 14:58:14 +0100

    start logic

---
+++ tictactoe.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+// functions
+void resetBoard();
+void printBoard();
+void checkFreeSpaces();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner(char);
+
+// global vars
+char board[3][3];
+const char PLAYER = 'X';
+const char COMPUTER = 'O';
+
+
+int main() {
+    //
+    
+
+    //
+    return 0;
+}
+
+
+void resetBoard() {
+
+}
+
+void printBoard() {
+
+}
+
+void checkFreeSpaces() {
+
+}
+
+void playerMove() {
+
+}
+
+void computerMove() {
+
+}
+
+char checkWinner() {
+
+}
+
+void printWinner(char winner_char) {
+
+}
