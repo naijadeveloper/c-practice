4c23450121f702f4cdcb7bb6e1d62196f154298e
Enoch Enujiugha <naijadeveloper@gmail.com>
2025-06-23 14:09:34 +0100

    FUNCTION to find the maximum value in an array of integers

--- main.c
+++ main.c
@@ -1,26 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 
 
-int find_min_array(int array[], int length) {
-    int min = array[0];
-
-    for(int i = 0; i < length; i++) {
-        if(array[i] < min) {
-            min = array[i];
-        }
-    }
-
-    return min;
-}
-
-void bubble_sort() {
-    int array[] = {5, 9, 10, 11, 4, 3, 8, 6, 7};
-    int size_of_array = sizeof(array)/sizeof(array[0]);
-
+void bubble_sort(int array[], int length) {
     // ascending sort
-    for(int i = 0; i < size_of_array - 1; i++) {
-        for(int j = 0; j < size_of_array - 1 - i; j++) {
+    for(int i = 0; i < length - 1; i++) {
+        for(int j = 0; j < length - 1 - i; j++) {
             if(array[j+1] < array[j]) { // for ascending use <, for descending use >
                 // swap
                 // printf("found match betw: %d and %d", array[j], array[j+1]);
@@ -33,20 +17,48 @@ void bubble_sort() {
     }
 
     // print out sorted array
-    for(int i = 0; i < size_of_array; i++) {
+    for(int i = 0; i < length; i++) {
         printf("%d, ", array[i]);
     }
     puts("");
 }
 
+int find_min_array(int array[], int length) {
+    int min = array[0];
+
+    for(int i = 1; i < length; i++) {
+        if(array[i] < min) {
+            min = array[i];
+        }
+    }
+
+    return min;
+}
+
+int find_max_array(int array[], int length) {
+    int max = array[0];
+
+    for(int i = 1; i < length; i++) {
+        if(array[i] > max) {
+            max = array[i];
+        }
+    }
+
+    return max;
+}
+
 
 int main(void) {
-    // bubble_sort();
-
     int array[] = {5, 9, 10, 11, 4, 3, 8, 6, 7};
     int length = sizeof(array)/sizeof(array[0]);
-    int res = find_min_array(array, length);
-    printf("res: %d\n", res);
+
+    // 3) int res = find_max_array(array, length);
+    // printf("res: %d\n", res);
+
+    // 2) int res = find_min_array(array, length);
+    // printf("res: %d\n", res);
+
+    // 1) bubble_sort(array, length);
 
     //
     return 0;
